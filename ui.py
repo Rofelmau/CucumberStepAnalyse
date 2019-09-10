652872d1b07fb64843ee26790ecfb9b1a1202e29
Luca Schüller <luca.schueller@macio.de>
2019-09-10 09:30:35 +0200

    adding icon, resizing inputs

--- ui.py
+++ ui.py
@@ -7,29 +7,34 @@ class Ui:
 
     def __init__(self):
         root_window = Tk()
-        root_window.title("Step Analyse")
+        root_window.title("Step Definition Scanner")
+        root_window.iconbitmap('icon.ico')
+        root_window.grid_columnconfigure(2, weight=1)
+        root_window.grid_rowconfigure(1, weight=1)
+        root_window.grid_rowconfigure(2, weight=1)
+        root_window.grid_rowconfigure(3, weight=1)
 
-        Label(root_window, text="ROOT_PATH: ").grid(row=1, column=1, sticky=W)
+        Label(root_window, text="ROOT_PATH: ").grid(row=1, column=1, sticky=E)
 
         self.root_path = Entry(root_window)
-        self.root_path.grid(row=1, column=2, columnspan=2)
+        self.root_path.grid(row=1, column=2, columnspan=2, sticky="news")
 
-        Label(root_window, text="FILE_POSTFIX: ").grid(row=2, column=1, sticky=W)
+        Label(root_window, text="FILE_POSTFIX: ").grid(row=2, column=1, sticky=E)
 
         self.file_postfix = Entry(root_window)
-        self.file_postfix.grid(row=2, column=2, columnspan=2)
+        self.file_postfix.grid(row=2, column=2, columnspan=2, sticky="news")
 
-        Label(root_window, text=" ").grid(row=0, column=0, sticky=W)
-        Label(root_window, text=" ").grid(row=3, column=1, sticky=W)
+        Label(root_window, text=" ").grid(row=0, column=0, sticky="news")
+        Label(root_window, text=" ").grid(row=3, column=1, sticky="news")
         self.result_string = StringVar()
         self.result_string.set("")
         Label(root_window, textvariable=self.result_string).grid(row=4, column=1, columnspan=3)
-        Label(root_window, text=" ").grid(row=5, column=1, sticky=W)
-        Label(root_window, text=" ").grid(row=7, column=6, sticky=W)
+        Label(root_window, text=" ").grid(row=5, column=1, sticky="news")
+        Label(root_window, text=" ").grid(row=7, column=6, sticky="news")
 
         ttk.Style().configure('green/black.TButton', foreground='black', background='black')
         b = ttk.Button(root_window, text="Run Scanner", command=self._run_scanner, style='green/black.TButton')
-        b.grid(row=6, column=1, columnspan=3)
+        b.grid(row=6, column=1, columnspan=3, sticky="news")
 
         window_width = root_window.winfo_reqwidth()
         window_height = root_window.winfo_reqheight()
